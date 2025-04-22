630201d274ab41edacf64494c8fe1840b00e03a0
9five7 <trucpro1282003@gmail.com>
2025-04-22 18:18:51 +0700

    xử lý ảnh với sharp

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,21 +1,22 @@
 import { Request } from 'express'
-import formidable from 'formidable'
+import formidable, { File } from 'formidable'
 import fs from 'fs'
 import path from 'path'
+import { UPLOAD_TEMP_DIR } from '~/constants/dir'
 export const initFolder = () => {
-  const uploadFolderPath = path.resolve('src/uploads')
-  if (!fs.existsSync(uploadFolderPath)) {
-    fs.mkdirSync(uploadFolderPath, {
+
+  if (!fs.existsSync(UPLOAD_TEMP_DIR)) {
+    fs.mkdirSync(UPLOAD_TEMP_DIR, {
       recursive: true //muc dich la de tao folder nested
     })
   }
 }
 export const handleUploadSingleImage = async (req: Request) => {
   const form = formidable({
-    uploadDir: path.resolve('src/uploads'),
+    uploadDir: UPLOAD_TEMP_DIR,
     maxFiles: 1,
     keepExtensions: true,
-    maxFileSize: 600 * 1024, // 600kb,
+    maxFileSize: 4000 * 1024, // 600kb,
     filter: function ({ name, originalFilename, mimetype }) {
       console.log({ name, originalFilename, mimetype })
       const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
@@ -26,7 +27,7 @@ export const handleUploadSingleImage = async (req: Request) => {
     }
   })
 
-  return new Promise((resolve, reject) => {
+  return new Promise<File>((resolve, reject) => {
     form.parse(req, (err, fields, files) => {
       if (err) {
         return reject(err)
@@ -35,7 +36,12 @@ export const handleUploadSingleImage = async (req: Request) => {
       if (!Boolean(files.image)) {
         return reject(new Error('File is empty'))
       }
-      resolve(files)
+      resolve((files.image as File[])[0])
     })
   })
+}
+export const getNameFromFullname = (fullname: string) => {
+  const namearr = fullname.split('.')
+  namearr.pop()
+  return namearr.join('')
 }
